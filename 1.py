93e027db8e18efa1a2de2de5e540e1281da9991c
Swarali Yadav <102916059+LeeCodess@users.noreply.github.com>
2022-09-22 18:55:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+name=input("what is your name?\n")
+print("hi"+" "+name)
+
+city=input("which city are you from?\n")
+print(city+" "+"is a beautiful city")
+
+pet=input("what would you name your pet?\n")
+print("i hope "+pet+" "+"is a cat, hahaha")
+
+print(name+", your band name is"+" "+city+" "+pet+"!")
