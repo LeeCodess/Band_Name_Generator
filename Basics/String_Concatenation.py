1061fa3e208685e546cdcf2f6626b46179126f1a
Swarali Yadav <102916059+LeeCodess@users.noreply.github.com>
2022-09-22 19:05:31 +0530

    Add files via upload

---
+++ Basics/String_Concatenation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# project 1 - generating a band name using string concatenation
+
+
+# In[2]:
+
+
+# using the print() function
+
+
+# In[4]:
+
+
+print("Welcome to LeeCodess :)")
+
+
+# In[5]:
+
+
+# using the input() function
+
+
+# In[6]:
+
+
+name=input("what is your name?\n")
+print("hi"+" "+name)
+
+
+# In[7]:
+
+
+city=input("which city are you from?\n")
+print(city+" "+"is a beautiful city")
+
+
+# In[8]:
+
+
+pet=input("what would you name your pet?\n")
+print("i hope "+pet+" "+"is a cat, hahaha")
+
+
+# In[9]:
+
+
+print(name+", your band name is"+" "+city+" "+pet+"!")
+
+
+# In[10]:
+
+
+print("end")
+
